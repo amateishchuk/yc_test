26efd9f907eb377d0475d77e19fa3c99a54b4317
amateishchuk <amateishchuk@gmail.com>
2018-03-06 20:19:49 +0200

    remove useless usings

--- yc_test/Program.cs
+++ yc_test/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace yc_test
 {
--- yc_test/Singletons/ClientStorage.cs
+++ yc_test/Singletons/ClientStorage.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net;
-using System.Text;
 
 namespace yc_test
 {
